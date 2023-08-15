d6990f1931717b71fafeebef09dac3b5bd0296c1
elisagc <elisagcubero@gmail.com>
2023-08-15 14:12:59 +0200

    feat: table

---
+++ src/components/Table.tsx
@@ -0,0 +1,85 @@
+import {
+  Avatar,
+  Box,
+  Table as MUITable,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { PodcastColumn } from "../models/podcast";
+import "./Table.css";
+
+interface TableProps {
+  columns: PodcastColumn[];
+  rows: any[];
+}
+
+interface TableCardProps {
+  uri: string;
+  title: string;
+  subtitle: string;
+}
+
+interface TableLabelProps {
+  label?: string;
+  className?: string;
+}
+
+export const TableCard = ({ uri, title, subtitle }: TableCardProps) => {
+  return (
+    <Box className="card-container">
+      <Avatar src={uri} variant="rounded" />
+      <Box>
+        <Typography className="ellipsis one" variant="body2">
+          {title}
+        </Typography>
+        <Typography className="ellipsis one">{subtitle}</Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export const TableLabel = ({ label, className }: TableLabelProps) => {
+  return (
+    <Typography variant="body1" className={className}>
+      {label}
+    </Typography>
+  );
+};
+
+const Table = ({ columns, rows }: TableProps) => {
+  return (
+    <TableContainer>
+      <MUITable>
+        <TableHead>
+          <TableRow>
+            {columns.map((column, index) => (
+              <TableCell key={index}>
+                <Typography variant="body1">{column.header}</Typography>
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row, rowIndex) => (
+            <TableRow
+              key={rowIndex}
+              sx={{
+                "&:last-child td, &:last-child th": { border: 0 },
+              }}
+            >
+              {columns.map((column, index) => (
+                <TableCell key={index}>{column.render(row)}</TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </MUITable>
+    </TableContainer>
+  );
+};
+
+export default Table;
